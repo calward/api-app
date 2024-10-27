11b4969d2bb4130fef173f694cd8a619643920e0
Cole Alward <cole@shiftdsm.com>
2024-10-26 22:01:15 -0500

    Authenticate against API key

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::API
+  before_action :authenticate
+
+  def authenticate
+    api_key = request.headers['HTTP_AUTHORIZATION'].gsub('Bearer ', '')
+    valid_key = APIKey.find_by(value: api_key)
+    unless valid_key
+      render json: { data: 'No valid API key provided' }, status: :unauthorized
+    end
+  end
 end
